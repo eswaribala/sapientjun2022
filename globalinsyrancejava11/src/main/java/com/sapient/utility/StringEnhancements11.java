c7812688b7a7dcddb3ab890ce4912c719178e591
eswaribala <parameswaribala@gmail.com>
2022-06-22 11:48:12 +0530

    updated

--- globalinsyrancejava11/src/main/java/com/sapient/utility/StringEnhancements11.java
+++ globalinsyrancejava11/src/main/java/com/sapient/utility/StringEnhancements11.java
@@ -1,13 +1,23 @@
 package com.sapient.utility;
 
+import java.util.List;
 import java.util.ResourceBundle;
+import java.util.stream.Collectors;
 
 public class StringEnhancements11 {
 public static void main(String[] args){
   //test string in property file
 
     ResourceBundle resource=ResourceBundle.getBundle("db");
-    System.out.println(resource.getString("url"));
+    //java11
+    //isblank
+    System.out.println(resource.getString("url").isBlank());
+    System.out.println(resource.getString("url").isEmpty());
+    System.out.println(resource.getString("url").repeat(3));
+    List<String> values=resource.getString("data")
+            .lines().collect(Collectors.toList());
+    values.stream().forEach(System.out::println);
+
 
 }
 }
