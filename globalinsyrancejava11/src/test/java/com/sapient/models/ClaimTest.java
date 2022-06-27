da3dc77c32fa28e90daa0c25e927dbb1bc6e8f41
eswaribala <parameswaribala@gmail.com>
2022-06-27 15:21:50 +0530

    updated

---
+++ globalinsyrancejava11/src/test/java/com/sapient/models/ClaimTest.java
@@ -0,0 +1,43 @@
+package com.sapient.models;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.time.LocalDate;
+
+public class ClaimTest {
+
+    private static Claim claim;
+
+    @BeforeAll
+    public static void createClaimInstance(){
+        claim=new Claim();
+    }
+
+    @Test
+    @DisplayName("Claim Not Null Test")
+    public void claimNotNullTest(){
+        assertNotNull(claim);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {428578,3597349,3570837,0})
+    public void claimIdNotZeroTest(int data){
+
+        claim.setClaimId(data);
+        assertTrue(claim.getClaimId()>0);
+    }
+    @ParameterizedTest
+    @ValueSource(strings = {"2022-01-02","2022-03-04","2021-09-19"})
+    public void claimDateNotCurrentDateTest(String data){
+
+         claim.setClaimDate(LocalDate.parse(data));
+         assertTrue(LocalDate.now().isAfter(claim.getClaimDate()));
+    }
+
+}
