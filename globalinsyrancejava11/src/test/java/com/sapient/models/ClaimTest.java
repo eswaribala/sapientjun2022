3a22797c97c9a9cbcd3ff56ef0a73db2c9c5d761
eswaribala <parameswaribala@gmail.com>
2022-06-27 15:32:10 +0530

    updated

--- globalinsyrancejava11/src/test/java/com/sapient/models/ClaimTest.java
+++ globalinsyrancejava11/src/test/java/com/sapient/models/ClaimTest.java
@@ -4,11 +4,14 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Timeout;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.time.LocalDate;
+import java.util.concurrent.TimeUnit;
 
 public class ClaimTest {
 
@@ -39,5 +42,19 @@ public class ClaimTest {
          claim.setClaimDate(LocalDate.parse(data));
          assertTrue(LocalDate.now().isAfter(claim.getClaimDate()));
     }
+    @ParameterizedTest
+    @Timeout(unit = TimeUnit.NANOSECONDS,value = 5)
+    @CsvFileSource(resources = "./claims.csv", numLinesToSkip = 1)
+    void testWithCsvFileSource(int claimId,int policyNo,long claimAmount,LocalDate claimDate) {
+
+        claim.setClaimId(claimId);
+        claim.setPolicyNo(policyNo);
+        claim.setClaimAmount(claimAmount);
+        claim.setClaimDate(claimDate);
+        assertTrue(claim.getClaimAmount()>20000);
+        
+    }
+
+
 
 }
