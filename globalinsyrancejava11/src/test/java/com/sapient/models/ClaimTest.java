84bff44322440f18afe24fe3c6ad437d9712183e
eswaribala <parameswaribala@gmail.com>
2022-06-28 09:50:55 +0530

    updated

--- globalinsyrancejava11/src/test/java/com/sapient/models/ClaimTest.java
+++ globalinsyrancejava11/src/test/java/com/sapient/models/ClaimTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.condition.*;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -21,6 +22,8 @@ public class ClaimTest {
     }
 
     @Test
+    @RepeatedTest(5)
+   // @DisabledOnOs(OS.WINDOWS)
     @DisplayName("Claim Not Null Test")
     @Tag("dev")
     public void claimNotNullTest(){
@@ -57,6 +60,75 @@ public class ClaimTest {
 
     }
 
+    @Test
+    @EnabledOnOs(OS.MAC)
+    void testOnMacOs() {
+        assertTrue(true);
+    }
+
+    @Test
+    @DisabledOnOs(OS.WINDOWS)
+    void doNotTestOnWindows() {
+        assertTrue(true);
+    }
+
+    @Test
+    @DisabledOnJre(JRE.JAVA_8)
+    void onlyOnJava8() {
+        assertTrue(true);
+    }
+
+    @Test
+    @EnabledOnJre({ JRE.JAVA_17, JRE.JAVA_18 })
+    void onJava17Or18() {
+        assertTrue(true);
+    }
+
+    @Test
+    @DisabledForJreRange(min = JRE.JAVA_8, max = JRE.JAVA_11)
+    void notFromJava8to11() {
+        assertTrue(true);
+    }
+
+    @Test
+    @EnabledForJreRange(min = JRE.JAVA_12, max = JRE.JAVA_18)
+    void fromJava12to18() {
+        assertTrue(true);
+    }
+
+    @Test
+    @EnabledIfSystemProperty(named = "any.system.property", matches = "value-regex*")
+    public void onlyIfPropertyValueIsFound() {
+        assertTrue(true);
+    }
+
+    @Test
+    @EnabledIfEnvironmentVariable(named = "ENV", matches = ".*development.*")
+    public void executeOnlyInDevEnvironment() {
+        assertTrue(true);
+    }
+
+    @Test
+    @DisabledIfEnvironmentVariable(named = "ENV", matches = ".*prod.*")
+    public void disabledOnProdEnvironment() {
+        assertTrue(true);
+    }
+
+    @Test
+    @EnabledIf("customConditionalFunction")
+    void enabled() {
+        assertTrue(true);
+    }
+
+    @Test
+    @DisabledIf("customConditionalFunction")
+    void disabled() {
+        assertTrue(true);
+    }
+
+    boolean customConditionalFunction() {
+        return true;
+    }
 
 
 }
