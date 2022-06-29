4650d4b2b3ca03bd099c43aeb4a6ff3ae9ab0e0b
eswaribala <parameswaribala@gmail.com>
2022-06-29 16:59:36 +0530

    updated

---
+++ globalinsurance/scripts/index.js
@@ -0,0 +1,11 @@
+window.addEventListener('load',function (){
+
+    var element=document.querySelector("#root");
+
+
+        var article = document.createElement("article");
+        article.className = "article";
+        element.appendChild(article);
+
+
+})
