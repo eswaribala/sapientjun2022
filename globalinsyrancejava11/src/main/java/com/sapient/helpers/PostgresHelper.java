bde666f92771694aceaca721793e8380d6d49e69
eswaribala <parameswaribala@gmail.com>
2022-06-29 09:42:03 +0530

    updated

---
+++ globalinsyrancejava11/src/main/java/com/sapient/helpers/PostgresHelper.java
@@ -0,0 +1,25 @@
+package com.sapient.helpers;
+
+import com.mongodb.MongoClient;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+
+public class PostgresHelper {
+
+    private static ResourceBundle resourceBundle;
+    private static String url;
+    private static String userName;
+    private static String password;
+
+
+    public static Connection getConnection() throws SQLException {
+        resourceBundle=ResourceBundle.getBundle("db");
+        url=resourceBundle.getString("postgresUrl");
+        userName=resourceBundle.getString("postgresUserName");
+        password=resourceBundle.getString("postgresPassword");
+        return DriverManager.getConnection(url,userName,password);
+    }
+}
