7d55e5f52a20333aa3444de716c6bdc0e98e67a1
eswaribala <parameswaribala@gmail.com>
2022-06-30 13:48:59 +0530

    updated

---
+++ globalinsurance/scripts/claimform.js
@@ -0,0 +1,8 @@
+window.addEventListener('load',function(){
+
+    var claimBtn=document.querySelector("#claimBtn");
+    claimBtn.addEventListener('click',function(){
+        alert("form submitted...");
+    })
+
+})
