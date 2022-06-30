c0226b590cb226ba1398b021e145a1a005c07774
eswaribala <parameswaribala@gmail.com>
2022-06-30 10:12:48 +0530

    updated

--- globalinsurance/scripts/index.js
+++ globalinsurance/scripts/index.js
@@ -1,5 +1,11 @@
 window.addEventListener('load',function (){
 
+    var COLORS = [];
+    while(COLORS.length < 100){
+        COLORS.push(`rgb(${rand(0,255)},${rand(0,255)},${rand(0,255)})`);
+    }
+    console.log(COLORS);
+    let k=0;
     var element=document.querySelector("#root");
     var table=document.createElement("table");
     for(let i=0;i<10;i++){
@@ -8,6 +14,8 @@ window.addEventListener('load',function (){
             var td=document.createElement("td");
             var article = document.createElement("article");
             article.className = "article";
+            article.style.backgroundColor=COLORS[k];
+            k++;
             td.appendChild(article);
             tr.appendChild(td);
         }
@@ -15,3 +23,7 @@ window.addEventListener('load',function (){
     }
     element.appendChild(table);
 })
+
+function rand(from,to){
+    return ~~((Math.random()*(to-from))+from);
+}
