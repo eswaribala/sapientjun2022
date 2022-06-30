6ca7d9515f81176272c82c35ef8432b47b38744b
eswaribala <parameswaribala@gmail.com>
2022-06-30 14:03:46 +0530

    updated

--- globalinsurance/scripts/claimform.js
+++ globalinsurance/scripts/claimform.js
@@ -1,8 +1,29 @@
 window.addEventListener('load',function(){
 
-    var form=document.querySelector("form");
+    form=document.querySelector("form");
     form.addEventListener('submit',function(){
        console.log("form submitted");
+       claimId=document.querySelector("#claimId").value;
+       policyNo=document.querySelector("#policyNo").value;
+       claimDate=document.querySelector("#claimDate").value;
+       email=document.querySelector("#email").value;
+       mobileNo=document.querySelector("#mobileNo").value;
+       driverType=document.querySelector("#driverType").value;
+       driverName=document.querySelector("#driverName").value;
+
+       //create json object
+        claimObject={
+            "claimId":claimId,
+            "policyNo":policyNo,
+            "mobileNo":mobileNo,
+            "email":email,
+            "claimDate":claimDate,
+            "driverType":driverType,
+            "driverName":driverName
+        }
+
+        console.log(claimObject);
+
         return false;
     })
 
