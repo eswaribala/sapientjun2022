94c2e27939c0d2856bc61287495ef2506b7ca47d
eswaribala <parameswaribala@gmail.com>
2022-06-30 14:41:58 +0530

    updated

--- globalinsurance/scripts/viewclaim.js
+++ globalinsurance/scripts/viewclaim.js
@@ -1,7 +1,43 @@
 window.addEventListener('load',function(){
 
+    var table=document.querySelector("#claimtbl");
+
+    tr=document.createElement("tr");
+
     if(window.localStorage.getItem("claim")!=undefined){
-        console.log(window.localStorage.getItem("claim"));
+        claimObject=JSON.parse(window.localStorage.getItem("claim"));
+        console.log(claimObject.claimId);
+        td=document.createElement("td");
+        text=document.createTextNode(claimObject.claimId);
+        td.appendChild(text);
+        tr.appendChild(td);
+        td=document.createElement("td");
+        text=document.createTextNode(claimObject.policyNo);
+        td.appendChild(text);
+        tr.appendChild(td);
+        td=document.createElement("td");
+        text=document.createTextNode(claimObject.claimDate);
+        td.appendChild(text);
+        tr.appendChild(td);
+        td=document.createElement("td");
+        text=document.createTextNode(claimObject.email);
+        td.appendChild(text);
+        tr.appendChild(td);
+        td=document.createElement("td");
+        text=document.createTextNode(claimObject.mobileNo);
+        td.appendChild(text);
+        tr.appendChild(td);
+        td=document.createElement("td");
+        text=document.createTextNode(claimObject.driverType);
+        td.appendChild(text);
+        tr.appendChild(td);
+        td=document.createElement("td");
+        text=document.createTextNode(claimObject.driverName);
+        td.appendChild(text);
+        tr.appendChild(td);
+
     }
+    table.appendChild(tr);
+
 
 })
