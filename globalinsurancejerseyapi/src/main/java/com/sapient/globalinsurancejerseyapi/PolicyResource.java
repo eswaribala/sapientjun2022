1ecee625a0736c78308a8dcad8982ab97e5bd905
eswaribala <parameswaribala@gmail.com>
2022-07-19 14:48:27 +0530

    updated

--- globalinsurancejerseyapi/src/main/java/com/sapient/globalinsurancejerseyapi/PolicyResource.java
+++ globalinsurancejerseyapi/src/main/java/com/sapient/globalinsurancejerseyapi/PolicyResource.java
@@ -4,9 +4,7 @@ import com.sapient.globalinsurancejerseyapi.dao.PolicyImpl;
 import com.sapient.globalinsurancejerseyapi.facades.PolicyFacade;
 import com.sapient.globalinsurancejerseyapi.models.Policy;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.GenericEntity;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -46,4 +44,35 @@ public class PolicyResource {
                 .build();
 
     }
+
+    @POST
+    @Consumes({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+    @Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
+
+    public Response addPolicy(Policy policy){
+        boolean status=false;
+
+        try {
+            PolicyFacade policyFacade=new PolicyImpl();
+           status= policyFacade.addPolicy(policy);
+
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        return Response
+                .status(200)
+                .header("Access-Control-Allow-Origin", "*")
+                .header("Access-Control-Allow-Headers", "origin, content-type, accept, authorization")
+                .header("Access-Control-Allow-Credentials", "true")
+                .header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD")
+                .header("Access-Control-Max-Age", "1209600")
+                .entity(status)
+                .build();
+
+
+    }
+
 }
