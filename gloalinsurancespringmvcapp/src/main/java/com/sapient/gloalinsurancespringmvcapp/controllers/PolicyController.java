3f55f87e20d07ab3f2a6944a90c2136c67fd6460
eswaribala <parameswaribala@gmail.com>
2022-07-20 17:17:12 +0530

    updated

--- gloalinsurancespringmvcapp/src/main/java/com/sapient/gloalinsurancespringmvcapp/controllers/PolicyController.java
+++ gloalinsurancespringmvcapp/src/main/java/com/sapient/gloalinsurancespringmvcapp/controllers/PolicyController.java
@@ -1,6 +1,8 @@
 package com.sapient.gloalinsurancespringmvcapp.controllers;
 
 import com.sapient.gloalinsurancespringmvcapp.models.Policy;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -12,7 +14,7 @@ import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class PolicyController {
-
+    private static final Logger logger = LoggerFactory.getLogger(PolicyController.class);
     @GetMapping("/")
     public String loadHome(){
         return "index";
@@ -26,6 +28,8 @@ public class PolicyController {
 
     @PostMapping("policies")
     public String savePolicy(@ModelAttribute("policy") @Validated Policy policy, BindingResult result, Model model) {
+
+        logger.info("Policy Value as,"+ policy);
         return "success";
     }
 }
