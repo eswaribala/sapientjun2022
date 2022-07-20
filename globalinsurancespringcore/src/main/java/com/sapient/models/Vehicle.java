a264cde96c87638bc54aa7518a6336e1bf45a50e
eswaribala <parameswaribala@gmail.com>
2022-07-20 14:55:57 +0530

    updated

--- globalinsurancespringcore/src/main/java/com/sapient/models/Vehicle.java
+++ globalinsurancespringcore/src/main/java/com/sapient/models/Vehicle.java
@@ -1,8 +1,13 @@
 package com.sapient.models;
 
+import com.sapient.utility.ApplicationContextApp;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.BeanClassLoaderAware;
+import org.springframework.beans.factory.BeanNameAware;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
@@ -11,11 +16,22 @@ import java.util.Date;
 @AllArgsConstructor
 @NoArgsConstructor
 @Component
-public class Vehicle {
-
+public class Vehicle implements BeanNameAware, BeanClassLoaderAware {
+    private static final Logger logger = LoggerFactory.getLogger(ApplicationContextApp.class);
     private String regNo;
     private String make;
     private String color;
     private long cost;
     private Date dop;
+
+
+    @Override
+    public void setBeanName(String s) {
+        logger.info("Bean Aware"+s);
+    }
+
+    @Override
+    public void setBeanClassLoader(ClassLoader classLoader) {
+        logger.info("BeanClassLoader"+ classLoader.getName());
+    }
 }
