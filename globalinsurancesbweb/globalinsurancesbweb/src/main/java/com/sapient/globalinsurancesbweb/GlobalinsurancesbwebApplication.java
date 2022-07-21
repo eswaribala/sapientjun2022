c4107495f3523a9700459dc2e9df5c1fc4a2d2f3
eswaribala <parameswaribala@gmail.com>
2022-07-21 17:43:29 +0530

    updated

--- globalinsurancesbweb/globalinsurancesbweb/src/main/java/com/sapient/globalinsurancesbweb/GlobalinsurancesbwebApplication.java
+++ globalinsurancesbweb/globalinsurancesbweb/src/main/java/com/sapient/globalinsurancesbweb/GlobalinsurancesbwebApplication.java
@@ -2,6 +2,8 @@ package com.sapient.globalinsurancesbweb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class GlobalinsurancesbwebApplication {
@@ -9,5 +11,12 @@ public class GlobalinsurancesbwebApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GlobalinsurancesbwebApplication.class, args);
 	}
+
+
+	@Bean
+	public RestTemplate createRestTemplate(){
+		return new RestTemplate();
+	}
+
 
 }
--- globalinsurancesbweb/globalinsurancesbweb/src/main/java/com/sapient/globalinsurancesbweb/controllers/OwnerController.java
+++ globalinsurancesbweb/globalinsurancesbweb/src/main/java/com/sapient/globalinsurancesbweb/controllers/OwnerController.java
@@ -3,16 +3,21 @@ package com.sapient.globalinsurancesbweb.controllers;
 
 import com.sapient.globalinsurancesbweb.models.Owner;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 
 @Controller
 
 public class OwnerController {
+    @Autowired
+    private RestTemplate restTemplate;
+    @Value("${url}")
+    private String url;
 
 
     @GetMapping("/")
@@ -24,8 +29,29 @@ public class OwnerController {
 
 
     @PostMapping("/owners")
-    public String loginValidation(@ModelAttribute("owner") Owner owner) {
+    public String addOwner(@ModelAttribute("owner") Owner owner) {
 
-         return "success";
+
+        if(owner.getMobileNo()>0) {
+
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_JSON);
+            HttpEntity request = new HttpEntity<>(owner,headers);
+
+
+            ResponseEntity<?> authResponse=restTemplate.
+                    postForEntity(url+"/owners/v1.0",request, String.class);
+
+               System.out.println(authResponse.getBody());
+                return  "success";
+
+
+        }
+        else
+
+          return "redirect:/";
+
     }
+
+
 }
