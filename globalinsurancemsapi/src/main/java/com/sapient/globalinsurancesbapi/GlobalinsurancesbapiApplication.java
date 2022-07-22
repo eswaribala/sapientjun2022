ff201c92b81a5032fcf34f8937332c17aa6ef29c
eswaribala <parameswaribala@gmail.com>
2022-07-22 16:28:40 +0530

    updated

--- globalinsurancemsapi/src/main/java/com/sapient/globalinsurancesbapi/GlobalinsurancesbapiApplication.java
+++ globalinsurancemsapi/src/main/java/com/sapient/globalinsurancesbapi/GlobalinsurancesbapiApplication.java
@@ -4,10 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableAutoConfiguration(exclude = DataSourceAutoConfiguration.class)
-
+@EnableEurekaClient
 public class GlobalinsurancesbapiApplication {
 
     public static void main(String[] args) {
