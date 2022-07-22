98bae47847d036942b1874c824ce94be08f3a7c9
eswaribala <parameswaribala@gmail.com>
2022-07-22 16:11:34 +0530

    updated

---
+++ pseurekaserver/src/test/java/com/ps/pseurekaserver/PseurekaserverApplicationTests.java
@@ -0,0 +1,14 @@
+package com.ps.pseurekaserver;
+/*
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PseurekaserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+*/
