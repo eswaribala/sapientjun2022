56579d6f1ede25916aa58c247b3c6554d192fbe8
eswaribala <parameswaribala@gmail.com>
2022-09-06 22:15:58 +0530

    updated

--- globalinsurancereactjs/globalinsuranceapi/server.js
+++ globalinsurancereactjs/globalinsuranceapi/server.js
@@ -4,6 +4,7 @@ var bodyParser =require("body-parser");
 var cors=require("cors");
 const log = require("log-to-file");
 var app = express();
+const elasticsearch = require("elasticsearch")
 app.use(cors());
 //app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json())
@@ -11,6 +12,10 @@ app.use(bodyParser.json())
 app.get("/policies", function(request, response) {
     response.send("Testing Express ....!");
 });
+
+const esClient = elasticsearch.Client({
+    host: "http://3.230.135.37:9200",
+})
 app.post('/policy',function(request,response)
 {
     console.log("Testing...")
@@ -30,8 +35,23 @@ app.post('/writetolog',function(request,response)
     console.log("Testing...")
     response.setHeader('Access-Control-Allow-Origin', '*');
     response.setHeader('Access-Control-Allow-Methods', 'GET, POST');
-    response.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type");
+    response.header("Access-Control-Allow-Headers", "application/json, Content-Type");
     response.setHeader('Access-Control-Allow-Credentials', true);
+    esClient.index({
+        index: 'products2022',
+        body: {
+            "id": request.body.id,
+            "name": request.body.name,
+            "price": request.body.price,
+            "description": request.body.description,
+        }
+    })
+        .then(response => {
+            return response.json({"message": "Indexing successful"})
+        })
+        .catch(err => {
+            return response.status(500).json({"message": "Error"})
+        })
     console.log(request.body.data);
 
 
